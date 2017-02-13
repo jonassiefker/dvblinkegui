531a88957df6660a637aa39754def2d5cc54d0a7
Jonas Siefker <jonas.siefker@tis.biz>
2017-02-13 10:31:04 +0100

    Initial commit for DVBLink Enhanced Graphical User Interface (DVBLink EGUI)

---
+++ src/main/java/dvblink/egui/DVBLinkEGUI.java
@@ -0,0 +1,14 @@
+package dvblink.egui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by siefker on 13.02.2017.
+ */
+@SpringBootApplication
+public class DVBLinkEGUI {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DVBLinkEGUI.class, args);
+    }
+}
